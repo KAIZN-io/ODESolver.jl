f7ba150f421427ea3ae4ef6a143d8e91b5ba61b5
Jan-Luca Klees <email@janlucaklees.de>
2019-08-24 15:39:50 +0200

    Dummy test implementatino for the lorenz system
    In this commit we added example data for the lorenx system to be used as
    a test. We still need to wire in the example results to be able to
    really compare what our simulations does.

--- src/ODESolver.jl
+++ src/ODESolver.jl
@@ -1,7 +1,13 @@
 module ODESolver
 
-function solveODE()
-  return [];
+function solveODE(start, stop, stepSize, model, initialValues, parameters)
+  return Dict(
+  "t" => collect(0:0.1:30),
+  "x" => collect(0:0.1:30),
+  "y" => collect(0:0.1:30),
+  "z" => collect(0:0.1:30),
+)
+
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,37 @@
 using Test: @test
 using ODESolver: solveODE
 
+start = 0
+stop = 30
+stepSize = 0.1
 
-@test solveODE() == []
+model = Dict(
+  "algebraic"=>[
+    Dict("var"=>"c", "orres"=>["x^2"]),
+  ],
+  "differential"=>[
+    Dict("var"=>"x", "orres"=>["sigma * (y - x)"]),
+    Dict("var"=>"y", "orres"=>["x * (rho - z) - y"]),
+    Dict("var"=>"z", "orres"=>["x * y - beta * z"]),
+  ]
+)
+
+initialValues = [
+  Dict("var"=>"x", "orres"=>"0.1"),
+  Dict("var"=>"y", "orres"=>"0"),
+  Dict("var"=>"z", "orres"=>"0"),
+]
+
+parameters = [
+  Dict("var"=>"sigma", "orres"=>"10"),
+  Dict("var"=>"rho",   "orres"=>"28"),
+  Dict("var"=>"beta",  "orres"=>"8 / 3"),
+]
+
+@test solveODE(start, stop, stepSize, model, initialValues, parameters) == Dict(
+  "t" => collect(0:0.1:30),
+  "x" => collect(0:0.1:30),
+  "y" => collect(0:0.1:30),
+  "z" => collect(0:0.1:30),
+)
 
